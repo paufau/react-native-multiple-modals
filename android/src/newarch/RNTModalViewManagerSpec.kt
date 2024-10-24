e3233c5fc8bb8b9663006a54fc798d1182075df1
paufau <pavelpakseev@MacBook-Pro-Pavel.local>
2024-10-24 20:28:59 +0400

    Add Android new architecture support

---
+++ android/src/newarch/RNTModalViewManagerSpec.kt
@@ -0,0 +1,21 @@
+package com.multiplemodals
+
+import android.view.ViewGroup
+
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewGroupManager
+import com.facebook.react.uimanager.ViewManagerDelegate
+import com.facebook.react.viewmanagers.RNTModalViewManagerDelegate
+import com.facebook.react.viewmanagers.RNTModalViewManagerInterface
+
+abstract class RNTModalViewManagerSpec<T : ViewGroup> : ViewGroupManager<T>(), RNTModalViewManagerInterface<T> {
+  private val mDelegate: ViewManagerDelegate<T>
+
+  init {
+    mDelegate = RNTModalViewManagerDelegate(this)
+  }
+
+  override fun getDelegate(): ViewManagerDelegate<T>? {
+    return mDelegate
+  }
+}
