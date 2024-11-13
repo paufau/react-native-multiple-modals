97024d859f434eaebe66a652d0022739d2702f9b
paufau <pakseev.1999@gmail.com>
2024-11-13 21:13:57 +0400

    iOS Paper Shadow View (#13)
    * Add android shadow views
    
    * Add android paper shadow view

---
+++ ios/RNTModalShadowView.mm
@@ -0,0 +1,17 @@
+#import "RNTModalShadowView.h"
+
+#import "RCTBridge.h"
+#import "RCTShadowView.h"
+#import "RCTUtils.h"
+
+@implementation RNTModalShadowView
+
+- (void)insertReactSubview:(id<RCTComponent>)subview atIndex:(NSInteger)atIndex
+{
+    [super insertReactSubview:subview atIndex:atIndex];
+    if ([subview isKindOfClass:[RCTShadowView class]]) {
+        ((RCTShadowView *)subview).size = RCTScreenSize();
+    }
+}
+
+@end
