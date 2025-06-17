4bc3a93075fb48e5a480241e784afd7e73c91761
paufau <pakseev.1999@gmail.com>
2025-06-17 23:27:12 +0400

    Update version & fix .h, .m ios files

--- ios/Library/RNTModalWindowHelper/RNTModalWindowHelper.m
+++ ios/Library/RNTModalWindowHelper/RNTModalWindowHelper.m
@@ -1,18 +1,36 @@
-#ifndef RNTModalWindowHelper_h
-#define RNTModalWindowHelper_h
-
 #import <UIKit/UIKit.h>
+#import "RNTModalWindowHelper.h"
 
-@protocol RNTModalWindowHelperProtocol <NSObject>
+@implementation RNTModalWindowHelper
 
-- (UIWindow *_Nullable)getKeyWindow;
-- (UIViewController *_Nullable)getRootController;
-- (UIInterfaceOrientation)getWindowOrientation;
+- (UIWindow *)getKeyWindow
+{
+    for (UIScene *scene in [UIApplication sharedApplication].connectedScenes)
+    {
+        if ([scene isKindOfClass:[UIWindowScene class]])
+        {
+            UIWindowScene *windowScene = (UIWindowScene *)scene;
+            for (UIWindow *window in windowScene.windows)
+            {
+                if (window.isKeyWindow)
+                {
+                    return window;
+                }
+            }
+        }
+    }
+
+    return nil;
+}
+
+- (UIViewController *)getRootController
+{
+    return [self getKeyWindow].rootViewController;
+}
+
+- (UIInterfaceOrientation)getWindowOrientation
+{
+    return [self getKeyWindow].windowScene.interfaceOrientation ?: UIInterfaceOrientationPortrait;
+}
 
 @end
-
-@interface RNTModalWindowHelper : NSObject <RNTModalWindowHelperProtocol>
-
-@end
-
-#endif /* RNTModalWindowHelper_h */
