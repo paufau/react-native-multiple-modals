51a52ae19d447d43fa5df4babcf937a76de0efcd
paufau <pakseev.1999@gmail.com>
2025-10-06 23:19:59 +0400

    Fix layout inspector

--- src/LayoutInspectorProvider.tsx
+++ src/LayoutInspectorProvider.tsx
@@ -6,7 +6,9 @@ import { RootTagContext } from 'react-native/Libraries/ReactNative/RootTag';
 // Wrap the children in AppContainer so the React Native inspector works
 // within the modal. See https://github.com/facebook/react-native/blob/v0.81.4/packages/react-native/Libraries/Modal/Modal.js#L308
 // for the inspiration within the RN Modal component.
-export const AppContainerWrapper: FC<PropsWithChildren> = ({ children }) => {
+export const LayoutInspectorProvider: FC<PropsWithChildren> = ({
+  children,
+}) => {
   const rootTag = useContext(RootTagContext);
 
   // The inspector is only needed in development
