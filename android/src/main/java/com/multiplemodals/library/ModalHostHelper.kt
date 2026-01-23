fcf60065061cc2a0f161a93824808b55d49f5ed4
Wendy <w.dekock@programic.com>
2026-01-23 14:31:10 +0100

    feat(heightCalculation) calculate height with getRealSize

--- android/src/main/java/com/multiplemodals/library/ModalHostHelper.kt
+++ android/src/main/java/com/multiplemodals/library/ModalHostHelper.kt
@@ -16,31 +16,9 @@ internal object ModalHostHelper {
     fun getModalHostSize(context: Context, includeStatusBar: Boolean): Point {
         val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
         val display = Assertions.assertNotNull(wm).defaultDisplay
-        // getCurrentSizeRange will return the min and max width and height that the window can be
-        display.getCurrentSizeRange(MIN_POINT, MAX_POINT)
-        // getSize will return the dimensions of the screen in its current orientation
-        display.getSize(SIZE_POINT)
-
-        val attrs = intArrayOf(R.attr.windowFullscreen)
-        val theme = context.theme
-        val ta = theme.obtainStyledAttributes(attrs)
-        val windowFullscreen = ta.getBoolean(0, false)
-
-        // We need to add the status bar height to the height if we have a fullscreen window,
-        // because Display.getCurrentSizeRange doesn't include it.
-        val resources = context.resources
-        val statusBarId = resources.getIdentifier("status_bar_height", "dimen", "android")
-        var statusBarHeight = 0
-        if ((windowFullscreen || includeStatusBar) && statusBarId > 0) {
-            statusBarHeight = resources.getDimension(statusBarId).toInt()
-        }
-
-        return if (SIZE_POINT.x < SIZE_POINT.y) {
-            // If we are vertical the width value comes from min width and height comes from max height
-            Point(MIN_POINT.x, MAX_POINT.y + statusBarHeight)
-        } else {
-            // If we are horizontal the width value comes from max width and height comes from min height
-            Point(MAX_POINT.x, MIN_POINT.y + statusBarHeight)
-        }
+        
+        display.getRealSize(SIZE_POINT)
+        
+        return SIZE_POINT
     }
 }
