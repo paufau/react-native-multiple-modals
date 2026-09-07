d5040b83fb7ce939875be6df30a5a200f0b913f9
paufau <pakseev.1999@gmail.com>
2026-09-07 16:01:35 +0400

    Remove unused eslint ignore

--- src/LayoutInspectorProvider.tsx
+++ src/LayoutInspectorProvider.tsx
@@ -18,7 +18,6 @@ export const LayoutInspectorProvider: FC<PropsWithChildren> = ({
   // Lazy import to avoid bundling AppContainer in production builds.
   // AppContainer is an internal React Native module that may not be
   // available in production builds on Android, causing crashes.
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const AppContainer = require('react-native/Libraries/ReactNative/AppContainer').default;
 
   return <AppContainer rootTag={rootTag}>{children}</AppContainer>;
