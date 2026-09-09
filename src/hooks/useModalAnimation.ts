24ed4e0de703a1b505a2bd94cb56cb609f43b522
paufau <pakseev.1999@gmail.com>
2026-09-09 13:57:59 +0400

    fix(web): fix formatting

--- src/hooks/useModalAnimation.ts
+++ src/hooks/useModalAnimation.ts
@@ -1,5 +1,5 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import type { RefObject } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import type { View } from 'react-native';
 
 import {
